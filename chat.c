4a051f8de1477688f271755646d2d749002395c3
Arthur Assis <arthur120496@gmail.com>
2019-05-07 11:36:18 -0300

    Creates create queue function

--- chat.c
+++ chat.c
@@ -7,30 +7,40 @@
 #include<string.h>
 #include<unistd.h>
 
+//gcc -o mq-send chat.c -lrt
+//TODO discover the mesage size
 #define YOSHIDA "/yoshida"
 #define ARTHUR "/arthur"
 #define MSG_SIZE 30
 #define MAX_MSG 10
 
-//gcc -o mq-send chat.c -lrt
-//TODO discover the mesage size
+
+//TODO permissions
+//TODO handle queue name conflicts
+mqd_t create(char* name){
+    mqd_t queue;
+    struct mq_attr attr;    
+    attr.mq_maxmsg = MAX_MSG;
+    attr.mq_msgsize = sizeof(char)*MSG_SIZE;
+    attr.mq_flags = 0;
+    if((queue=mq_open(name, O_CREAT, 0666, &attr)) < 0){
+        printf("deu ruiim\n");
+        exit(1);
+    }
+
+    return queue;
+}
+
+
 int main(){
     mqd_t queue;
-    struct mq_attr attr;
     char msg[MSG_SIZE];
     char received_msg[MSG_SIZE];
 
     strcpy(msg,"teste2");
     strcpy(received_msg,"nao recebi");
 
-    attr.mq_maxmsg = MAX_MSG;
-    attr.mq_msgsize = sizeof(msg) ;
-    attr.mq_flags = 0;
-
-    if((queue=mq_open(YOSHIDA, O_RDWR|O_CREAT, 0666, &attr)) < 0){
-        printf("deu ruiim\n");
-        exit(1);
-    }
+    queue = create (YOSHIDA);
 
     if(mq_send(queue, (void*) &msg,sizeof(msg), 0) < 0){
         printf("deu ruiim\n");
